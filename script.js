6a50f5df133f8709d0d324f5edd41aba3d99a7ab
t-hanafin <thanafin@outlook.com>
2023-01-31 16:41:02 -0500

    fix it so that the game is actually now best of five.

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// alert("This is working")
 
-// This function chooses rock, paper, or scissors at random.
+
+// This function chooses rock, paper, or scissors at random and assigns that choice to the computer.
+
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * (3 - 0) ) + 0;
-
-
 
     if (computerChoice == 0) {
         computerChoice = "rock";
@@ -19,11 +18,8 @@ function getComputerChoice() {
 
 }
 
-// Sets the player's selection to a default choice.
 
-
-
-// This function plays a round of RPS.
+// This function plays a single round of rock, paper, scissors.
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
@@ -37,57 +33,57 @@ function playRound(playerSelection, computerSelection) {
         case 'scissorsscissors':
         case 'paperpaper':
             winner = 0;
-            return(`It's a tie because we both chose ${playerSelection}.`);
+//            return(`It's a tie because we both chose ${playerSelection}.`);
             break;
         case 'paperrock':
         case 'scissorspaper':
         case 'rockscissors':
             winner = 1;
-            return(`You win because ${playerSelection} beats ${computerSelection}!`);
+//            return(`You win because ${playerSelection} beats ${computerSelection}!`);
             break;
         default:
             winner = 2;
-            return(`I win because ${computerSelection} beats ${playerSelection}!`);
+//            return(`I win because ${computerSelection} beats ${playerSelection}!`);
             break;
     }
 }
 
 function game() {
 
-    playerWins = 0;
-    computerWins = 0;
-    ties = 0;
+    let wins = 0;
+    let playerWins = 0;
+    let computerWins = 0;
 
-    for (let i = 0; i < 5; i++) {
+    while (playerWins < 3 && computerWins < 3) {
 
         var computerSelection = getComputerChoice();
-
-        var playerSelection = prompt("rock paper scissors MATCH! ");
-
+    
+        var playerSelection = prompt("rock paper scissors MATCH! ", "rock");
+    
+        playerSelection = playerSelection.toLowerCase();
+    
         playRound(playerSelection, computerSelection);
-
+    
         console.log(winner);
-
+    
         if (winner == 1) {
+            wins += 1;
             playerWins += 1;
             alert(`You win, ${playerSelection} beats ${computerSelection}! The score is now ${playerWins} for you and ${computerWins} for me.`);
         } else if (winner == 2) {
+            wins += 1;
             computerWins += 1;
             alert(`I win, ${computerSelection} beats ${playerSelection}! The score is now ${playerWins} for you and ${computerWins} for me.`);
         } else if (winner == 0) {
-            alert(`We both chose ${playerSelection}. The score is now ${playerWins} for you and ${computerWins} for me.`);
-            ties += 1;
+            alert(`We both chose ${playerSelection}. The score is still ${playerWins} for you and ${computerWins} for me.`);
         }
 
-        
     }
 
     if (computerWins > playerWins) {
         console.log(`Final result: I won, ${computerWins} games to ${playerWins}.`);
     } else if (computerWins < playerWins) {
         console.log(`Final result: you won, ${playerWins} games to ${computerWins}.`);
-    } else {
-        console.log(`Final result: it's a ${playerWins} - ${computerWins} tie.`);
     }
 
 
